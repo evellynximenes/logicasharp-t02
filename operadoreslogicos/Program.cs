674c69e31c60617a375d0b9364932457fd72081f
EVELLYN BARBARA GONCALVES ROCHA <evellyn.bgrocha@senacsp.edu.br>
2025-11-06 14:58:09 -0300

    atualizando

---
+++ operadoreslogicos/Program.cs
@@ -0,0 +1,12 @@
+﻿bool x = true;
+bool y = false;
+
+bool and = x && y;
+bool or = x || y;
+bool not = !x;
+
+//exibindo resultados dos operadores logicos 
+Console.WriteLine("\nOPERADORES LÓGICOS:");
+Console.WriteLine($"x && y: {and} ");
+Console.WriteLine($"x || y: {or}");
+Console.WriteLine($"!x: {not}");
---
+++ operadoresrelacinais/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 10;
+int b = 05;
+
+//operadores relacionais
+
+bool maiorQue = a > b;
+bool menorQue = a < b;
+bool igual = a == b;
+bool diferente = a != b;
+bool maiorOuIgual = a >= b;
+bool menorOuIgual = a <= b;
+
+
+//exibindo resultados dos operadores relacionais
+Console.WriteLine("\nOperadores Relacionais:");
+Console.WriteLine($"\n {a} > {b}: {maiorQue}");
+Console.WriteLine($"\n {a} < {b}: {menorQue}");
+Console.WriteLine($"\n {a} == {b}: {igual}");
+Console.WriteLine($"\n{a} != {b}: {diferente}");
+Console.WriteLine($"\n{a} >= {b}: {maiorOuIgual}");
+Console.WriteLine($"\n{a} <= {b}: {menorOuIgual}");
