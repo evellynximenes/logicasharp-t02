aac40ffa10370ce928de3d55b5ef414165aa43f2
EVELLYN BARBARA GONCALVES ROCHA <evellyn.bgrocha@senacsp.edu.br>
2025-11-11 14:12:06 -0300

    maratonadeexercicios01

---
+++ maratonaexercicio01/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+/* 1.Saudação com Nome
+Objetivo: Declarar variáveis, usar concatenação e interpolação.
+Descrição: Crie um script que declare uma variável para o nome de
+uma pessoa e exiba uma saudação personalizada, por exemplo: "Olá,
+João! Bem - vindo ao nosso site!".
+Dica: Utilize tanto a concatenação (.) quanto a interpolação dentro de
+uma string com aspas duplas.*/
+
+string Nome = "Ana Claudia";
+string barra = "-";
+
+Console.WriteLine($"Olá,{Nome}! Bem" + barra + "Vinda ao Nosso site!");
+
+
+
+/*2. Conversão de Temperatura (Celsius para Fahrenheit)
+Objetivo: Trabalhar com cálculos matemáticos e variáveis.
+Descrição: Crie um script que converta uma temperatura em Celsius
+para Fahrenheit. A fórmula para conversão é:
+Dica: Declare uma constante para armazenar o valor 9/5 e faça o
+cálculo com a variável fornecida.*/
+
+
+
+/*3. Impressão de Dados do Usuário
+Objetivo: Concatenar e interpolar dados.
+Descrição: Crie um script que solicite ao usuário seu nome, idade e
+cidade e depois mostre uma mensagem com essas informações.
+Exemplo: "Meu nome é João, tenho 25 anos e moro em São Paulo".
+Dica: Use tanto concatenação quanto interpolação.*/
+
+
+
+/*4. Empréstimo Bancário
+Objetivo: Cálculos financeiros e declaração de variáveis.
+Descrição: Crie um script que calcule o valor de uma parcela de um
+empréstimo, dado o valor total do empréstimo, a taxa de juros e o
+número de parcelas. Exemplo de fórmula:
+Dica: Utilize variáveis para o valor do empréstimo, a taxa de juros e o
+número de parcelas.*/
+
+
+/*5. Conversor de Moeda
+Objetivo: Trabalhar com variáveis, operadores e arredondamento.
+Descrição: Crie um script que converta uma quantia em reais para
+dólares, com base em uma taxa de câmbio fornecida. Mostre o valor
+convertido com 2 casas decimais.
+Dica: Use round() para arredondar o valor para 2 casas decimais.*/
+
+
+/*6 . Calculadora de Desconto
+Objetivo: Cálculos matemáticos e uso de variáveis.
+Descrição: Crie um script que calcule o valor do desconto de um
+produto, dado o preço original e a porcentagem de desconto. Exemplo
+de fórmula:
+Dica: Use variáveis para armazenar o preço e a porcentagem do
+desconto e calcule o valor final.*/
