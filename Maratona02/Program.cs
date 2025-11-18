36cb1c8aa4018fc4fe28b01d8accba78d94c0693
EVELLYN BARBARA GONCALVES ROCHA <evellyn.bgrocha@senacsp.edu.br>
2025-11-18 16:54:49 -0300

    atualizar

---
+++ Maratona02/Program.cs
@@ -0,0 +1,43 @@
+﻿// Exercícios com if, else e elseif:
+
+
+/*1.Faça um programa para verificação de estoque
+Um sistema de gerenciamento de estoque precisa notificar os funcionários 
+quando for hora de reabastecer um produto. 
+Escreva um código que verifique a quantidade de um produto em estoque. Se a 
+quantidade for menor que 5, exiba a mensagem: "Alerta: Baixo estoque. Por favor, 
+reabasteça este produto.". Caso contrário, exiba: "Estoque suficiente.".*/
+
+/*2. Faça um programa de desconto
+Uma loja oferece um desconto para compras acima de um certo valor como parte 
+de uma promoção de vendas. Escreva um código que determine o valor total da 
+compra de um cliente. Se o valor total for superior a R$ 200,00, aplique um 
+desconto de 10% e exiba: "Desconto de 10% aplicado!".Se o valor total for R$ 
+200,00 ou menos, informe: "Adicione mais itens ao carrinho para ganhar um 
+desconto de 10%.". 
+Exercícios com switch:
+
+1.Faça um programa que determine o dia da semana
+O programa deve receber um número de 1 a 7, representando um dia da semana,
+e imprimir o nome do dia correspondente (por exemplo, 1 = "Domingo", 2 = 
+"Segunda-feira", etc.).
+
+2. Faça um programa que calcule o preço final de um produto com base em um 
+código de desconto
+O programa deve receber um código de desconto (de 1 a 3) e aplicar o desconto 
+correspondente ao preço do produto.
+o Código 1: 10 % de desconto
+o Código 2: 20 % de desconto
+o Código 3: 30 % de desconto
+o Se o código for inválido, deve mostrar uma mensagem de erro.*/
+
+double preco = 100;
+double desconto1 = 1;
+
+
+switch (desconto1)
+    case 1:
+    Console.WriteLine($"{preco}")
+
+
+
---
+++ vetoresmatrizes/Program.cs
@@ -0,0 +1,46 @@
+﻿// declaração de dois vetores
+int[] numeros = { 7, 2, 3, 34, 5 }; //vetor de inteiros
+string[] nome = { "ana", "joão", "maria", "carlos" }; //vetor de strings 
+
+//exibindo os elementos do vetor de numeros com loop for
+Console.WriteLine("vetor de numeros (usando for):");
+for (int i = 0; i< numeros.Length; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+// exibindo elementos de vetor com lista foreach 
+Console.WriteLine("\nvetor de numeros ordenados:");
+foreach (var nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+
+//ordenação vetor numeros
+Array.Sort(numeros);
+Console.WriteLine("\nvetor de numeros ordenados:");
+foreach  (var n in numeros
+{
+    Console.WriteLine(n);
+}
+
+int valorProcurado = 5;
+int posicaoSequencial = PesquisaSequencial(numeros, valorProcurado);
+
+if (posicaoSequencial != -1)
+{
+    Console.WriteLine($"\nPesquisa Sequencial: valor {valorProcurado} encontrado na posição {posicaoSequencial}.");
+}
+else 
+{
+    Console.WriteLine($"\nPesquisa Sequencial: valor {valorProcurado} não encontrado.");
+}
+
+static int PesquisaSequencial(int[] vetor. int valor)
+{
+    for  (int i = 0; i< vetor.Length; i++)
+    { if (vetor[i] == valor)
+            return i; //retorna o indice do valor encontrado
+    }
+    return -1; //retorna -1 se não encontrar
+}
